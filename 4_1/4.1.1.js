39969c689fa48fb56df44811221e3a1cee1ac8e7
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-08-28 18:15:07 -0400

    criado programa para somar 2 variáveis

--- 4_1/4.1.1.js
+++ 4_1/4.1.1.js
@@ -1,6 +1,13 @@
-/*Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+/* Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Faça programas para:
 Adição
 Subtracão
 Multiplicação
 Divisão
 Módulo */
+
+const a = 4;
+const b = 11;
+
+// Adição
+
+console.log("A soma de " + a + " + " + b + " é: " + (a+b));
