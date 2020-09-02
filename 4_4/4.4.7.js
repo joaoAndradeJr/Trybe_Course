bece3c4b7d085c9904f7215e251a4640a528304a
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-01 22:00:34 -0400

    programa virou função como pedia o exercício

--- 4_4/4.4.7.js
+++ 4_4/4.4.7.js
@@ -3,19 +3,22 @@ Array de teste: [2, 3, 6, 7, 10, 1];.
 Valor esperado no retorno da função: 4. */
 
 let array = [2, 3, 6, 7, 10, 1];
-let maior = null;
-let indice = null;
 
-for(let i = 0; i < array.length; i += 1){
-    if(array[i] > maior){
-        maior = array[i];
+function indiceMaiorValor(){
+    let maior = null;
+    let indice = null;
+
+    for(let i = 0; i < array.length; i += 1){
+        if(array[i] > maior){
+            maior = array[i];
+        }
     }
-}
 
-for(let i = 0; i < array.length; i += 1){
-    if(array[i] === maior){
-        indice = i;
-    } 
+    for(let i = 0; i < array.length; i += 1){
+        if(array[i] === maior){
+            indice = i;
+        } 
+    }
+    return indice;
 }
-
-console.log(indice);
+console.log(indiceMaiorValor(array));
