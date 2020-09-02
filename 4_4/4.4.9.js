b09f7492fa5b45a4e0068f15ffa0331bda0b34d0
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-01 21:48:36 -0400

    programa recebe array de nomes e retorna o maior nome

---
+++ 4_4/4.4.9.js
@@ -0,0 +1,17 @@
+/* Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];.
+Valor esperado no retorno da função: Fernanda. */
+
+let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+let contador = 0;
+let maiorNome = '';
+
+for(let i = 0; i < array.length; i += 1){
+    contador = array[i].length;
+    if(contador > maiorNome.length){
+        maiorNome = array[i];
+    }
+}
+
+console.log(maiorNome);
