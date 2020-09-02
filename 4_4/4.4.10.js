90f5764be5bccd63fcd301fc2b588f1dab14b006
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-09-02 08:31:25 -0400

    programa verifica qual número mais se repete dentro do array

--- 4_4/4.4.10.js
+++ 4_4/4.4.10.js
@@ -2,3 +2,28 @@
 Array de teste: [2, 3, 2, 5, 8, 2, 3];.
 Valor esperado no retorno da função: 2. */
 
+let array = [2, 3, 2, 5, 8, 2, 3];
+
+let contador = 0;
+let contadorAnterior = 0;
+let maisRepetido = null;
+let numeroAtual = null;
+
+for(let i = 0; i < array.length; i += 1){
+    numeroAtual = array[i];
+    for(let j = 0; j < array.length; j += 1){
+        if(array[j] == numeroAtual){
+            contador += 1;
+        }
+    }
+    if(contadorAnterior == 0){
+        contadorAnterior = contador;
+        maisRepetido = numeroAtual;
+    }
+    if(contador > contadorAnterior){
+        maisRepetido = numeroAtual;
+    }
+    contador = 0;
+}
+
+console.log(maisRepetido);
