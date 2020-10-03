d8a38ebe9bde9481f6b96ffd5b2b6aa15efac4f2
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-10-03 11:41:37 -0400

    exercicio 4 parte 2

--- 7_1/4.js
+++ 7_1/4.js
@@ -24,3 +24,19 @@ const letraX = (palavra) => {
 }
 
 console.log(letraX('Bebeto'));
+
+/* Um array com escopo global, que é o escopo do arquivo JS, nesse caso, contendo cinco strings com suas principais skills.
+
+Função 2: Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string. Exemplo: "Tryber x aqui! Minhas cinco principais habilidades são:
+JavaScript;
+HTML; ...
+#goTrybe".*/
+
+const skills = ['HTML', 'CSS', 'JavaScript', 'POO', 'Java'];
+
+const concatenar = (param) => {
+  const retornoletraX = letraX('Bebeto');
+  return `${retornoletraX} Minhas cinco principais habilidades são: ${skills.sort()}`;
+}
+
+console.log(concatenar());
