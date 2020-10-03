31754129f47804b5b700690c2a07a50d42a60e54
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-10-03 09:38:09 -0400

    exercicio 2

---
+++ 7_1/1.js
@@ -0,0 +1,13 @@
+/* Crie uma função que receba um número e retorne seu fatorial.
+Na matemática, o fatorial de um número não negativo N, com a notação N!, é o produto de todos os inteiros menores ou iguais a N. Exemplo: 4! = 4 3 2 * 1 = 24.
+Bônus (opcional): tente fazer o mesmo exercício de forma recursiva. Spoiler: É possível resolver com uma linha */
+
+const fatorial = (num) => {
+  let resultado = 0;
+  for( let index = 1; index < num; index += 1) {
+    resultado += num * index;
+  }
+  return resultado;
+}
+
+console.log(fatorial(4));
---
+++ 7_1/2.js
@@ -0,0 +1,16 @@
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
+// Exemplo: longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") retorna 'aconteceu'
+
+const maiorPalavra = (frase) => {
+  const palavras = frase.split(' ');
+  let maiorPalavra = '';
+  for (let index = 0; index < palavras.length; index += 1) {
+    if (palavras[index].length > maiorPalavra.length) {
+      maiorPalavra = palavras[index];
+    }
+  }
+  return maiorPalavra;
+}
+
+
+console.log(maiorPalavra("Antônio foi no banheiro e não sabemos o que aconteceu"));
