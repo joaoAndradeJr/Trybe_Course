a336e92e907668576a86a873b69495536a817269
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-10-08 21:12:50 -0400

    exercicio 3

---
+++ 7_2/7.2.3.js
@@ -0,0 +1,25 @@
+// Para os exercícios a seguir, use o seguinte código.
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Crie uma função para mostrar o tamanho de um objeto
+
+const tamanhoObjeto = (objeto) => Object.keys(objeto).length;
