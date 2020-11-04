64bf43bda53e3dc4a26dc924c0e03bdb26c5b663
joaoAndradeJr <fullservicemanaus@gmail.com>
2020-11-04 15:11:49 -0400

    exercicios js assincrono 1

---
+++ javascript-assincrono-e-promises_1/ex1.js
@@ -0,0 +1,32 @@
+// Dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+`${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+name: "Mars",
+distanceFromSun: {
+  value: 227900000,
+  measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+name: "Venus",
+distanceFromSun: {
+  value: 108200000,
+  measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+name: "Jupiter",
+distanceFromSun: {
+  value: 778500000,
+  measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // Mars is 227900000 kilometers apart from the Sun
+console.log(planetDistanceFromSun(venus)); // Venus is 108200000 kilometers apart from the Sun
+console.log(planetDistanceFromSun(jupiter)); // Jupiter is 778500000 kilometers apart from the Sun
