12303a0d5b5621df15fc619c50015c1dd2aa3610
joaoAndradeJr <fullservicemanaus@gmail.com>
2020-11-07 20:00:41 -0400

    ex2 part 3 done

--- testes-automatizados-com-jest.1/ex2/reverseint/script.js
+++ testes-automatizados-com-jest.1/ex2/reverseint/script.js
@@ -8,8 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {
+function invertInt(int) {
+  const stringNumber = int.toString();
+  const size = stringNumber.length;
+  let invertedNumber = [];
+  if (int >= 0) {
+    for (let index = size; index >= 1; index -=1) {
+      invertedNumber.push(stringNumber[index-1]);
+    }
+    const saida = Number(invertedNumber.join(''));
+    return saida;
+  } 
+  for (let index = size; index >= 2; index -=1) {
+    invertedNumber.push(stringNumber[index-1]);
+  }
+  const saida = `-${(invertedNumber.join(''))}`;
+  return Number(saida);
+}
 
+function reverseInt(n) {
+  const result = invertInt(n);
+  return result;
 }
 
 module.exports = reverseInt;
